5df753956aac1c1b9be4c5a3bd7c981b74ae27d7
iHoHyeon <hyunee169@naver.com>
2022-01-28 23:55:03 +0900

    💄 [Design] 스타일 전역변수 생성

--- src/assets/styles/index.ts
+++ src/assets/styles/index.ts
@@ -3,6 +3,17 @@ import { normalize } from 'styled-normalize';
 
 const GlobalStyles = createGlobalStyle` 
   ${normalize}
+
+  :root {
+    --font-size-mid: 20px;
+    --font-size-base: 17px;
+    --font-size-small: 13px;
+
+    --color-blue: #2353BB;
+    --color-yellow: #f8d274;
+  }
+
+  font-size: var(--font-size-base);
 `;
 
 export default GlobalStyles;
