ebf56332a7414f1890a3d870a8698eed6681a27f
은배 <geb9211@gmail.com>
2022-01-29 00:12:11 +0900

    🚨 [Fix] eslint 기본 세팅 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,12 @@ module.exports = {
     project: './tsconfig.json',
   },
   rules: {
+    "prettier/prettier": ["error", {
+      "endOfLine":"auto"
+    }],
     '@typescript-eslint/semi': 'off',
     '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-constant-condition': 'off'
   },
 };
