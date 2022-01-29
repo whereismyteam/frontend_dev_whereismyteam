6b904db388a007bc66a3ee1db8ff01f359a329b5
iHoHyeon <hyunee169@naver.com>
2022-01-29 22:56:38 +0900

    💄 [Design] 모달 컴포넌트 애니매이션 추가

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,36 +1,84 @@
-import { ReactNode } from 'react';
-import styled from 'styled-components';
+import { ReactNode, useEffect, useState } from 'react';
+import styled, { css } from 'styled-components';
+
+import { fadeIn, fadeOut } from '../../assets/styles/keyframes';
 
 type ModalProps = {
   children?: ReactNode;
+  visible: boolean;
+  onClickClose: () => void;
 };
 
-const Background = styled.div`
+const modalSettings = (visible: boolean) => css`
+  visibility: ${visible ? 'visible' : 'hidden'};
+  z-index: 999;
+  animation: ${visible ? fadeIn : fadeOut} 0.3s ease-out;
+  transition: visibility 0.3s ease-out;
+`;
+
+const Background = styled.div<{ visible: boolean }>`
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  position: fixed;
+  position: absolute;
   background-color: rgba(41, 41, 41, 0.5);
-  z-index: 999;
+  ${(props) => modalSettings(props.visible)}
 `;
 
-const ModalSection = styled.div`
-  width: 800px;
+const ModalSection = styled.div<{ visible: boolean }>`
+  width: 768px;
+  height: 600px;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  border-radius: 50px;
   background-color: #fff;
   padding: 16px;
-  z-index: 999;
+  ${(props) => modalSettings(props.visible)}
 `;
 
-function Modal({ children }: ModalProps) {
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 30px 30px 0px 0px;
+  border: none;
+  background: none;
+  cursor: pointer;
+
+  font-size: var(--font-size-mid);
+  color: #888;
+`;
+
+function Modal({ children, visible, onClickClose }: ModalProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+    if (visible) {
+      setIsOpen(true);
+    } else {
+      timeoutId = setTimeout(() => setIsOpen(false), 200);
+    }
+
+    return () => {
+      timeoutId ?? clearTimeout(timeoutId);
+    };
+  }, [visible]);
+
+  if (!isOpen) return <></>;
+
   return (
     <>
-      <Background />
-      <ModalSection>{children}</ModalSection>
+      <Background visible={visible} onClick={onClickClose} />
+      <ModalSection visible={visible}>
+        <CloseButton type="button" onClick={onClickClose}>
+          X
+        </CloseButton>
+        {children}
+      </ModalSection>
     </>
   );
 }
