bc648659a1bab95066b2562fbe71a969610b8c0a
은배 <geb9211@gmail.com>
2022-02-02 00:50:33 +0900

    [Feat] auth 모달 로직 일부 구현

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,10 +1,7 @@
 import { useState } from 'react';
 
 import Modal from '../../components/common/modal';
-<<<<<<< Updated upstream
-=======
 import Login from './login';
->>>>>>> Stashed changes
 import Register from './register';
 
 interface AuthProps {
@@ -13,14 +10,11 @@ interface AuthProps {
 }
 
 function Auth({ setModalClose, visible }: AuthProps) {
-<<<<<<< Updated upstream
-=======
   const [isLogin, setIsLogin] = useState(true);
 
   const setRegister = () => setIsLogin((current) => !current);
 
   return <Modal children={isLogin ? <Login setRegister={setRegister} /> : <Register />} visible={visible} onClickClose={setModalClose}></Modal>;
->>>>>>> Stashed changes
 }
 
 export default Auth;
