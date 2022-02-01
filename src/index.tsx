d5f7ca710ff36c98ee6a06801084acf7e07cc940
은배 <geb9211@gmail.com>
2022-02-02 02:06:44 +0900

    ✨ [Feat] redux store 생성 및 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyle from './assets/styles';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
