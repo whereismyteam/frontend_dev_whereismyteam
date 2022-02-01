2b58fb4b59d9cd7d96e1a10d5561396286eab9a1
은배 <geb9211@gmail.com>
2022-02-02 02:05:39 +0900

    ✨ [Feat] auth redux 모듈 생성

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,11 +1,64 @@
-import { createStore } from 'react-redux';
+const MODAL_VISIBLE = 'auth/MODAL_VISIBLE';
+const SET_LOGIN = 'auth/SET_LOGIN';
+const SET_REGISTER = 'auth/SET_REGISTER';
+const SET_UID = 'auth/SET_UID';
+const SET_UPW = 'auth/SET_UPW';
 
-const initialState = {
-  modalVisibles: false,
+export const setModalVisible = (isVisible: boolean) => ({ type: MODAL_VISIBLE, isVisible });
+export const setLogin = () => ({ type: SET_LOGIN });
+export const setRegister = () => ({ type: SET_REGISTER });
+export const setUserId = (uid: string) => ({ type: SET_UID, uid });
+export const setUserPw = (upw: string) => ({ type: SET_UPW, upw });
+
+export type InitialState = {
+  modalVisible: boolean;
+  loginOrRegister: boolean;
+  userId: string;
+  userPw: string;
+};
+
+const initialState: InitialState = {
+  modalVisible: false,
   loginOrRegister: true,
   userId: '',
   userPw: '',
-
 };
 
-export {}
+type Action =
+  | { type: 'auth/MODAL_VISIBLE'; isVisible: boolean }
+  | { type: 'auth/SET_LOGIN' }
+  | { type: 'auth/SET_REGISTER' }
+  | { type: 'auth/SET_UID'; uid: string }
+  | { type: 'auth/SET_UPW'; upw: string };
+
+export default function auth(state = initialState, action: Action) {
+  switch (action.type) {
+    case MODAL_VISIBLE:
+      return {
+        ...state,
+        modalVisible: action.isVisible,
+      };
+    case SET_LOGIN:
+      return {
+        ...state,
+        loginOrRegister: true,
+      };
+    case SET_REGISTER:
+      return {
+        ...state,
+        loginOrRegister: false,
+      };
+    case SET_UID:
+      return {
+        ...state,
+        userId: action.uid,
+      };
+    case SET_UPW:
+      return {
+        ...state,
+        userId: action.upw,
+      };
+    default:
+      return state;
+  }
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,13 @@
-export {}
+import { combineReducers } from 'redux';
+import auth from './auth';
+import { InitialState } from './auth';
+
+export type rootState = {
+  auth: InitialState;
+};
+
+const rootReducer = combineReducers({
+  auth,
+});
+
+export default rootReducer;
