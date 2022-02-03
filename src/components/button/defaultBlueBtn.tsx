82f82c5c64cf470e4ecfb4a07f3ff4cacc2591e0
은배 <geb9211@gmail.com>
2022-02-04 02:21:15 +0900

    ♻️ [Refactor] 버튼 컴포넌트 이름 변경 및 수정

--- src/components/button/defaultBlueBtn.tsx
+++ src/components/button/defaultBlueBtn.tsx
@@ -1,40 +1,35 @@
 import styled from 'styled-components';
 
-// 기본 버튼에서 색상 반전된 버튼입니다.
+// 기본 파랑 색상 버튼입니다.
 // 버튼 사용시 btnName props에 버튼 이름을 전달해주면 됩니다.
 
 type BtnProps = {
   btnName: string;
   width: number;
   height: number;
-  marginTop?: string;
+  marginTop?: number;
 };
 
-const BtnWrapper = styled.div<{ width: number; height: number; marginTop?: string }>`
+const BtnWrapper = styled.div<{ width: number; height: number; marginTop?: number }>`
   display: flex;
   align-items: center;
   justify-content: center;
   width: ${(props) => props.width}px;
   height: ${(props) => props.height}px;
   margin-top: ${(props) => props.marginTop}px;
-  border: 2px solid var(--color-blue);
   border-radius: 30px;
   font-size: var(--font-size-base);
   font-weight: 400;
-  color: var(--color-blue);
-  background-color: #fff;
+  color: #fff;
+  background-color: var(--color-blue);
   cursor: pointer;
-  :hover {
-    color: #fff;
-    background-color: var(--color-blue);
-  }
 `;
 
 const Btn = styled.div`
   margin-top: 5px;
 `;
 
-function DefaultReverseButton({ btnName, width, height, marginTop }: BtnProps) {
+function DefaultBlueBtn({ btnName, width, height, marginTop }: BtnProps) {
   return (
     <BtnWrapper width={width} height={height} marginTop={marginTop}>
       <Btn>{btnName}</Btn>
@@ -42,4 +37,4 @@ function DefaultReverseButton({ btnName, width, height, marginTop }: BtnProps) {
   );
 }
 
-export default DefaultReverseButton;
+export default DefaultBlueBtn;
---
+++ src/components/button/defaultBlueInvBtn.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+// 기본 버튼에서 색상 반전된 버튼입니다.
+// 버튼 사용시 btnName props에 버튼 이름을 전달해주면 됩니다.
+
+type BtnProps = {
+  btnName: string;
+  width: number;
+  height: number;
+  marginTop?: number;
+};
+
+const BtnWrapper = styled.div<{ width: number; height: number; marginTop?: number }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  margin-top: ${(props) => props.marginTop}px;
+  border: 2px solid var(--color-blue);
+  border-radius: 30px;
+  font-size: var(--font-size-base);
+  font-weight: 400;
+  color: var(--color-blue);
+  background-color: #fff;
+  cursor: pointer;
+  :hover {
+    color: #fff;
+    background-color: var(--color-blue);
+  }
+`;
+
+const Btn = styled.div`
+  margin-top: 5px;
+`;
+
+function DefaultBlueInvBtn({ btnName, width, height, marginTop }: BtnProps) {
+  return (
+    <BtnWrapper width={width} height={height} marginTop={marginTop}>
+      <Btn>{btnName}</Btn>
+    </BtnWrapper>
+  );
+}
+
+export default DefaultBlueInvBtn;
