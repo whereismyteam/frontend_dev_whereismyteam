1b60f9fb6e435c4d090febb62db2c4e344e67ffa
iHoHyeon <hyunee169@naver.com>
2022-02-05 20:17:25 +0900

    🤡 [Test] 목업용 로그인 API 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,1 +1,7 @@
-export {}
+export const fetchLoginResult = async (email: string, password: string) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ result: 'success', userName: '9T' });
+    }, 500);
+  });
+};
