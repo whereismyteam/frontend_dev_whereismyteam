329eed46793342bc42a0afae946fc480a2407e84
iHoHyeon <hyunee169@naver.com>
2022-02-18 00:07:52 +0900

    💄 [Design] 메인 섹션 레이아웃 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Navigation from '../components/navigation';
 import Footer from '../components/footer';
 import Main from '../pages/main';
 import Post from '../pages/post';
 
+const Layout = styled.div`
+  margin-top: 100px;
+  margin-bottom: 40px;
+
+  height: calc(100vh - 140px);
+  overflow-y: scroll;
+`;
+
 function IndexRouter() {
   return (
     <BrowserRouter>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/post/:postId" element={<Post />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/post/:postId" element={<Post />} />
+        </Routes>
+      </Layout>
       <Footer />
     </BrowserRouter>
   );
