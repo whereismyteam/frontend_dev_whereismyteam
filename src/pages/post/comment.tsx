04c4aa84a7f5216e73d485a845d89200f95fa0a1
iHoHyeon <hyunee169@naver.com>
2022-02-18 00:09:27 +0900

    💄 [Design] 댓글 대댓글 레이아웃 작성

---
+++ src/pages/post/comment.tsx
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+
+import DefaultBlueBtn, { BtnWrapper } from '../../components/button/defaultBtn';
+import Reply from '../../assets/images/reply.svg';
+
+export interface IComment {
+  userImg: string;
+  userName: string;
+  text: string;
+  date: string;
+  isPrivate: boolean;
+  children: Array<{ userImg: string; userName: string; text: string; date: string; isPrivate: boolean }>;
+}
+
+interface CommentProps {
+  comment: IComment;
+}
+
+const CommentBox = styled.div`
+  position: relative;
+  padding: 20px;
+  width: calc(100% - 40px);
+  box-shadow: 3px 3px 5px 2px rgba(149, 149, 149, 0.5);
+  border-radius: 10px;
+`;
+
+const Layout = styled.div`
+  overflow-wrap: break-word;
+`;
+
+const ReplyWrapper = styled.div`
+  display: flex;
+`;
+
+const UserWrapper = styled.div`
+  display: flex;
+  align-self: flex-start;
+  align-items: center;
+`;
+const UserImg = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+`;
+const Text = styled.p`
+  font-size: 15px;
+  overflow-wrap: break-word;
+`;
+
+const EtcWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+const Date = styled.div`
+  color: #9d9d9d;
+`;
+
+const ReplyImg = styled.img`
+  width: 15px;
+  height: 15px;
+  margin-top: 40px;
+  padding-right: 10px;
+`;
+
+const ReplyBtn = styled(BtnWrapper)``;
+
+function CommentLayout({ userImg, userName, text, date, isPrivate }: { userImg: string; userName: string; text: string; date: string; isPrivate: boolean }) {
+  return (
+    <Layout>
+      <br />
+      <UserWrapper>
+        <UserImg src={userImg} />
+        &nbsp;&nbsp;&nbsp;{userName}&nbsp;&nbsp;&nbsp;{isPrivate && <div>(비밀)</div>}
+      </UserWrapper>
+      <br />
+      <Text>{text}</Text>
+      <EtcWrapper>
+        <Date>{date}</Date>
+        <DefaultBlueBtn btnName="답글" width={75} height={35} color="invBlue" />
+      </EtcWrapper>
+    </Layout>
+  );
+}
+
+function Comment({ comment }: CommentProps) {
+  return (
+    <>
+      <CommentBox>
+        <CommentLayout userImg={comment.userImg} userName={comment.userName} text={comment.text} date={comment.date} isPrivate={comment.isPrivate} />
+        {comment.children.map((reply) => (
+          <ReplyWrapper>
+            <ReplyImg src={Reply} alt="L" />
+            <CommentLayout userImg={reply.userImg} userName={reply.userName} text={reply.text} date={reply.date} isPrivate={reply.isPrivate} />
+          </ReplyWrapper>
+        ))}
+      </CommentBox>
+    </>
+  );
+}
+
+export default Comment;
