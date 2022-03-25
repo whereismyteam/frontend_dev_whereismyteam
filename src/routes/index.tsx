1338b76767b23656b6d6932c01728aa7dd5d3bf3
iHoHyeon <hyunee169@naver.com>
2022-03-25 14:48:36 +0900

    ✨ [Feat] 잘못된 경로 Redirect 설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Cookies } from 'react-cookie';
 import styled from 'styled-components';
 
@@ -50,6 +50,7 @@ function IndexRouter() {
           <Route path="/" element={<Main />} />
           <Route path="/social/google" element={<Social />} />
           <Route path="/post/:postId" element={<Post />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
       </Layout>
       <Footer />
